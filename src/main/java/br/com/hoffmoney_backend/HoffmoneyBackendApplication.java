1f4b8afd8e101a9672d0576ac70d41d6f4f0e95d
silvamelo510 <thyago.smm@gmail.com>
2024-07-24 23:27:21 -0300

    Configuração inicial

---
+++ src/main/java/br/com/hoffmoney_backend/HoffmoneyBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.hoffmoney_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoffmoneyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoffmoneyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hoffmoney_backend/HoffmoneyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hoffmoney_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoffmoneyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
