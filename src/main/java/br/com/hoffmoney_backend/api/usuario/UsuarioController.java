d734ca4c1b7db559ce7fc014300cd2eeee570b6b
Ronaldo Barbosa <ronaldo.s.barbosa@outlook.com>
2024-08-05 21:08:22 -0300

    tentanto implementar campo saldo do usuario

--- src/main/java/br/com/hoffmoney_backend/api/usuario/UsuarioController.java
+++ src/main/java/br/com/hoffmoney_backend/api/usuario/UsuarioController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import br.com.hoffmoney_backend.modelo.usuario.Usuario;
 import br.com.hoffmoney_backend.modelo.usuario.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,49 +17,39 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
-
-    @Operation(
-       summary = "Serviço responsável por salvar um usuário no sistema."
-    )
+        
+    @Operation(summary = "Serviço responsável por salvar um usuário no sistema.")
     @PostMapping
     public ResponseEntity<?> save(@RequestBody Usuario request) {
         if (request.getNome() == null || request.getNome().isEmpty() ||
-            request.getEmail() == null || request.getEmail().isEmpty() ||
-            request.getSenha() == null || request.getSenha().isEmpty()) {
+                request.getEmail() == null || request.getEmail().isEmpty() ||
+                request.getSenha() == null || request.getSenha().isEmpty()) {
             return new ResponseEntity<>("Todos os campos são obrigatórios.", HttpStatus.BAD_REQUEST);
         }
         Usuario usuario = usuarioService.save(request);
         return new ResponseEntity<>(usuario, HttpStatus.CREATED);
     }
 
-    @Operation(
-       summary = "Serviço responsável por listar todos os usuários do sistema."
-    )
+    @Operation(summary = "Serviço responsável por listar todos os usuários do sistema.")
     @GetMapping
     public List<Usuario> listAllUsers() {
         return usuarioService.listarTodos();
     }
 
-    @Operation(
-       summary = "Serviço responsável por listar um usuário específico do sistema."
-    )
+    @Operation(summary = "Serviço responsável por listar um usuário específico do sistema.")
     @GetMapping("/{id}")
     public Usuario getUserById(@PathVariable Long id) {
         return usuarioService.obterPorID(id);
     }
 
-    @Operation(
-       summary = "Serviço responsável por alterar dados de um usuário específico do sistema."
-    )
+    @Operation(summary = "Serviço responsável por alterar dados de um usuário específico do sistema.")
     @PutMapping("/{id}")
     public ResponseEntity<Usuario> updateUser(@PathVariable("id") Long id, @RequestBody Usuario request) {
         usuarioService.update(id, request);
         return ResponseEntity.ok().build();
     }
 
-    @Operation(
-       summary = "Serviço responsável por excluir um usuário específico do sistema."
-    )
+    @Operation(summary = "Serviço responsável por excluir um usuário específico do sistema.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         usuarioService.delete(id);
@@ -69,23 +60,37 @@ public class UsuarioController {
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
         try {
             Usuario usuario = usuarioService.login(loginRequest.getEmail(), loginRequest.getSenha());
-            
+
             if (usuario == null) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário não encontrado");
             }
-    
+
             if (!usuario.getSenha().equals(loginRequest.getSenha())) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Senha incorreta");
             }
-    
+
             Usuario logado = new Usuario();
             logado.setId(usuario.getId());
             logado.setNome(usuario.getNome());
             logado.setEmail(usuario.getEmail());
-    
+
             return ResponseEntity.ok(logado);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+    }
+     @Operation(summary = "Incrementar saldo da carteira do usuário.")
+    @PostMapping("/{id}/incrementar")
+    public ResponseEntity<?> incrementarSaldo(@PathVariable Long id, @RequestBody double valor) {
+        usuarioService.incrementarSaldo(id, valor);
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "Decrementar saldo da carteira do usuário.")
+    @PostMapping("/{id}/decrementar")
+    public ResponseEntity<?>  decrementarSaldo(@PathVariable Long id, @RequestBody double valor) {
+        usuarioService.decrementarSaldo(id, valor);
+        return ResponseEntity.ok().build();
+
     }
 }
--- src/main/java/br/com/hoffmoney_backend/modelo/usuario/UsuarioRepository.java
+++ src/main/java/br/com/hoffmoney_backend/modelo/usuario/UsuarioRepository.java
@@ -3,11 +3,19 @@ package br.com.hoffmoney_backend.modelo.usuario;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findById(Usuario usuarioId);
 
     Usuario findByEmail(String email);
+
+
+    @Modifying
+    @Query("UPDATE Usuario u SET u.saldo = u.saldo - :valor WHERE u.id = :usuarioId")
+    void decrementarSaldoPorIdUsuario(@Param("usuarioId") Long usuarioId, @Param("valor") Double valor);
 
 }  
