573be868cffbc31b6f9da34b8a48300c0b90b7e5
Vicente-Campos <vicentecampos344@gmail.com>
2024-08-28 11:28:26 -0300

    implementando o envio de e-mail

--- src/main/java/br/com/hoffmoney_backend/modelo/usuario/UsuarioService.java
+++ src/main/java/br/com/hoffmoney_backend/modelo/usuario/UsuarioService.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.hoffmoney_backend.modelo.mensagens.EmailService;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
 @Service
 public class UsuarioService {
+
+    @Autowired
+    private EmailService emailService;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -20,6 +24,10 @@ public class UsuarioService {
         usuario.setHabilitado(Boolean.TRUE);
         usuario.setVersao(1L);
         usuario.setDataCriacao(LocalDate.now());
+
+        // Comentar a linha abaixo quando não quiser mandar e-mail
+        emailService.enviarEmailConfirmacaoCadastroUsuario(usuario);
+
         return usuarioRepository.save(usuario);
     }
 
