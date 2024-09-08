f9ee9dc5412bfda991c45e8b87251d63b8b5c1e3
Vicente-Campos <vicentecampos344@gmail.com>
2024-09-08 19:18:43 -0300

    Validação nas entrada das requisições

--- src/main/java/br/com/hoffmoney_backend/api/despesa/DespesaRequest.java
+++ src/main/java/br/com/hoffmoney_backend/api/despesa/DespesaRequest.java
@@ -3,12 +3,16 @@ package br.com.hoffmoney_backend.api.despesa;
 import br.com.hoffmoney_backend.modelo.categoriadespesa.CategoriaDespesa;
 import br.com.hoffmoney_backend.modelo.despesa.Despesa;
 import br.com.hoffmoney_backend.modelo.usuario.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
@@ -17,11 +21,20 @@ import java.time.LocalDate;
 public class DespesaRequest {
 
     private Long idCategoriaDespesa;
+
     private Usuario usuario;
+
+    @NotBlank(message = "O nome da despesa precisa ser informado")
     private String nome;
+
+    @NotNull(message = "O valor da despesa precisa ser informado")
     private Double valor;
+
+    @Length(max = 200, message = "A descrição máxima é de {max} caracteres")
     private String descricao;
+
     private LocalDate dataDeCobranca;
+
     private Boolean paga;
 
     public Despesa build(CategoriaDespesa categoria) {
--- src/main/java/br/com/hoffmoney_backend/api/receita/ReceitaRequest.java
+++ src/main/java/br/com/hoffmoney_backend/api/receita/ReceitaRequest.java
@@ -1,9 +1,14 @@
 package br.com.hoffmoney_backend.api.receita;
 
 import java.time.LocalDate;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.hoffmoney_backend.modelo.categoriareceita.CategoriaReceita;
 import br.com.hoffmoney_backend.modelo.receita.Receita;
 import br.com.hoffmoney_backend.modelo.usuario.Usuario;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,11 +22,20 @@ import lombok.NoArgsConstructor;
 public class ReceitaRequest {
 
     private Long idCategoriaReceita;
+
     private Usuario usuario;
+
+    @NotBlank(message = "O nome da receita precisa ser informado")
     private String nome;
+
+    @NotNull(message = "O valor da receita precisa ser informado")
     private Double valor;
+
+    @Length(max = 200, message = "A descrição deverá ter no máximo {max} caracteres")
     private String descricao;
+
     private LocalDate dataDeCobranca;
+
     private Boolean paga;
 
     public Receita build(CategoriaReceita categoria) {
